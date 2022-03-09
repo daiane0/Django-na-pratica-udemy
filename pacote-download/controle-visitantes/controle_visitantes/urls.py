47734d7f8ce746273fbbb4eef664f7fab8d0868f
Daiane <daianemacedo001@gmail.com>
2022-03-09 15:15:06 -0300

    primeiro

---
+++ pacote-download/controle-visitantes/controle_visitantes/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path
+
+from usuarios.views import index
+from visitantes.views import registrar_visitante 
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+
+    path(
+        "",
+        index,
+        name="index",
+    ),
+
+    path(
+        "registrar-visitante/", #string que vai representar a url no navegador
+        registrar_visitante,
+        name="registrar-visitante"
+
+    )
+]
+
+    
